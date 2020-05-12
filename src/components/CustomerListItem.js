6825105593d3d7b9a0fa377928b42b2441b928cc
Poty <rivadeneiraie@gmail.com>
2020-05-12 19:44:09 -0300

    149 Creacion del componente CustomerListItem

---
+++ src/components/CustomerListItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const CustomerListItem = ({name, editAction, delAction, urlPath}) => {
+    return (
+        <div>
+            <div className='customers-list-item'>
+                <div className="">
+                    <Link to={`${urlPath}${dni}`}>{name}</Link>
+                </div>
+                <div className="">
+                    <Link to={`${urlPath}${dni}/edit`}>{editAction}</Link>
+                </div>
+                <div className="">
+                    <Link to={`${urlPath}${dni}/del`}>{delAction}</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+CustomerListItem.propTypes = {
+    name:PropTypes.string.isRequired,
+    editAction: PropTypes.string.isRequired,
+    delAction: PropTypes.string.isRequired,
+    urlPath:PropTypes.string.isRequired,
+};
+
+export default CustomerListItem;
