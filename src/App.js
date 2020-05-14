d92a842a6b2f1848b3951a4e6d9c6d339733c397
Poty <rivadeneiraie@gmail.com>
2020-05-14 12:09:42 -0300

    153 Utilizacion del componente Route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-          <Link to='/customers'>Customers</Link><br></br>
-          <Link to='/customers/30000000'>Customers 30000000</Link>
-      </div>
-    </Router>
-  );
+class App extends Component {
+
+  renderHome = () => <h1>Home</h1>;
+
+  renderCustomerContainer = () => <h1>Customer Container</h1>;
+
+  renderCustomerListContainer = () => <h1>Customers Lists Container</h1>;
+
+  renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
+
+  render() {
+      return (
+        <Router>
+          {/* 
+              <Link to='/customers'>Customers</Link><br></br>
+              <Link to='/customers/30000000'>Customers 30000000</Link>
+          </div> */}
+          <div className="App">
+            <Route exact path='/'               component={this.renderHome}></Route> 
+            <Route exact path='/customers'      component={this.renderCustomerListContainer}></Route> 
+            <Route exact path='/customers/:dni' component={this.renderCustomerContainer}></Route> 
+            <Route exact path='/customers/new'  component={this.renderCustomerNewContainer}></Route> 
+          </div>
+        </Router>
+      );
+    }
 }
 
 export default App;
