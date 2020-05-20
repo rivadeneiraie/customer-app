f8973edee41ed14a48f1cb021e6a24c373e08508
Poty <rivadeneiraie@gmail.com>
2020-05-20 13:46:38 -0300

    178 Instalamos ReduxForm lo agregamos como reducer y conectamos con ReduxForm

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as reduxForm} from 'redux-form';
 
 import { customers }  from './customers';
 
 export default combineReducers({
     customers,
+    form: reduxForm
 });
