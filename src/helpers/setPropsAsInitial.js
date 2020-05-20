2340b638a24b0a70cbe33a7d1c948654ff96ac37
Poty <rivadeneiraie@gmail.com>
2020-05-20 17:49:17 -0300

    181 Creacion del Higher Order Component para pasar el initialState a el componente de visualacion

---
+++ src/helpers/setPropsAsInitial.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export const setPropsAsInitial = WrapperComponents => (
+    class extends Component{
+        render(){
+            return <WrapperComponents {...this.props} initialValues={this.props}></WrapperComponents>
+        }
+    }
+);
