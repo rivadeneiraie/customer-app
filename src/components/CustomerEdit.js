ac1d2e9b255fdc83fc5c37c0380b5df3cae65b3e
Poty <rivadeneiraie@gmail.com>
2020-05-20 17:01:20 -0300

    179 Utilizacion de Field para agregar los campos al form

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
 
 const CustomerEdit = ({name, dni, age}) => {
     return (
         <div>
             <h2>Edición del cliente</h2>
-            <h3>Nombre: {name}/ Dni: {dni}/ Edad: {age}</h3>
+            <form action="">
+                <div>
+                    <label htmlFor="name">Nombre:</label>
+                    <Field
+                        name="name"
+                        component="input"
+                        type="text"
+                    ></Field>
+                </div>
+                <div>
+                    <label htmlFor="dni">Dni:</label>
+                    <Field
+                        name="dni"
+                        component="input"
+                        type="text"
+                    ></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Edad:</label>
+                    <Field
+                        name="age"
+                        component="input"
+                        type="number"
+                    ></Field>
+                </div>
+            </form>
         </div>
     );
 };
@@ -16,4 +42,4 @@ CustomerEdit.propTypes = {
     age:PropTypes.number,
 };
 
-export default CustomerEdit;
+export default reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
