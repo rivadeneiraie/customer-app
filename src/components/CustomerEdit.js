d07a8fa2ec3546b0e523bdcecee9d4028245dd6f
Poty <rivadeneiraie@gmail.com>
2020-05-29 10:48:22 -0300

    205 agregando la propiedad Ref para poder utilizar un elemento por medio del DOM

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from './../helpers/setPropsAsInitial';
@@ -9,7 +9,7 @@ const validate = values => {
     const error = {};
 
     if (!values.name ){
-        error.name = "El campo nombre es requerido";
+        error.name = "El    campo nombre es requerido";
     }
 
     if (!values.dni)
@@ -24,15 +24,7 @@ const isNumber = value => (
     isNaN(Number(value)) && "El campo debe ser un número"
 );
 
-const MyField = ({ input, meta, type, label, name }) => (
-    <div>
-        <label htmlFor={name}>{label}</label>
-        <input {...input} type={ !type ? "text" : type }></input>
-        {
-            meta.touched && meta.error && <span>{meta.error}</span> 
-        }
-    </div>
-);
+
 
 const toNumber = value => ( value && Number(value) )
 const toUpper = value => ( value && value.toUpperCase()  )
@@ -43,46 +35,71 @@ const onlyGrow = (value, previousValue, values) => (
 
 
 /** las propuedades pasadas como parametros son de redux-form tiene que lamarse así */
-const CustomerEdit = ({name, dni, age, handleSubmit, submitting, onBack, pristine, SubmitSucceeded}) => {
-    return (
+class CustomerEdit extends Component {
+
+    renderField = ({ input, meta, type, label, name, withFocus }) => (
         <div>
-            <h2>Edición del cliente</h2>
-            <form onSubmit={handleSubmit}> 
-                <Field
-                    name="name"
-                    component={MyField}
-                    type="text"
-                    label="Nombre:"
-                    parse={toUpper} //ambos de complementan
-                    format={toLower} //el primero es para guardar el otro para mostrar
-                ></Field>
-                <Field
-                    name="dni"
-                    component={MyField}
-                    type="text"
-                    label="Dni:"
-                ></Field>
-                <Field
-                    name="age"
-                    component={MyField}
-                    type="number"
-                    validate={isNumber}
-                    label="Edad:"
-                    parse={toNumber}
-                    normalize={onlyGrow} //funcion que se ejcuta posterior a parse
-                ></Field>
-                <CustomersActions>
-                    <button type="submit" disabled={pristine || submitting}>Aceptar</button>
-                    <button type="button" disabled={submitting} onClick={onBack}>Cancelar</button>
-                </CustomersActions>
-                <Prompt
-                    when={!pristine && !SubmitSucceeded} //el primero indica si el formulario cambio en algo y el segundo  si yua no se envió el formulario
-                    message={"Se perderán los datos si continúa"}
-                ></Prompt>
-            </form>
+            <label htmlFor={name}>{label}</label>
+            <input 
+                {...input} 
+                type={ !type ? "text" : type }
+                ref={withFocus && (txt => this.txt = txt) }>    
+            </input>
+            {
+                meta.touched && meta.error && <span>{meta.error}</span> 
+            }
         </div>
     );
-};
+
+    componentDidMount(){
+        if (this.txt)
+            this.txt.focus();
+    }
+
+    render() {
+        const { handleSubmit, submitting, onBack, pristine, SubmitSucceeded} = this.props;
+        return (
+            <div>
+                <h2>Edición del cliente</h2>
+                <form onSubmit={handleSubmit}> 
+                    <Field
+                        name="name"
+                        component={this.renderField}
+                        type="text"
+                        label="Nombre:"
+                        parse={toUpper} //ambos de complementan
+                        format={toLower} //el primero es para guardar el otro para mostrar
+                        withFocus
+                    ></Field>
+                    <Field
+                        name="dni"
+                        component={this.renderField}
+                        type="text"
+                        label="Dni:"   
+                    ></Field>
+                    <Field
+                        name="age"
+                        component={this.renderField}
+                        type="number"
+                        validate={isNumber}
+                        label="Edad:"
+                        parse={toNumber}
+                        normalize={onlyGrow} //funcion que se ejcuta posterior a parse
+                    ></Field>
+                    <CustomersActions>
+                        <button type="submit" disabled={pristine || submitting}>Aceptar</button>
+                        <button type="button" disabled={submitting} onClick={onBack}>Cancelar</button>
+                    </CustomersActions>
+                    <Prompt
+                        when={!pristine && !SubmitSucceeded} //el primero indica si el formulario cambio en algo y el segundo  si yua no se envió el formulario
+                        message={"Se perderán los datos si continúa"}
+                    ></Prompt>
+                </form>
+            </div>
+        );
+    }
+}
+
 
 CustomerEdit.propTypes = {
     name:PropTypes.string,
